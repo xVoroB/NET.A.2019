1b36525509edbb0ab1ef2b502c323e4feeaade54
Vlad Borovsky <xvorob@gmail.com>
2019-08-11 16:47:56 +0300

    Completed day 2 tasks

---
+++ NET.A.2019.Borovsky.02/NET.A.2019.Borovsky.02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FindNextBiggerNumber;
+using InsertNumber;
+using FilterDigit;
+using FindNthRoot;
+
+namespace NET.A._2019.Borovsky._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Finding next number");
+
+            int a = 1234321;
+            Console.WriteLine(FNBN.FindNextBiggerNumber(a));
+
+            Console.WriteLine("Inserting number");
+
+            Console.WriteLine(InsNum.InsertNumber(8, 15, 3, 8));
+
+            Console.WriteLine("FilterDigit");
+
+            List<int> arr = new List<int> { 1, 1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17 };
+            arr = FilDig.FilterDigit(arr);
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Finding Nth root");
+
+            Console.WriteLine(NthRoot.FindNthRoot(8, 3, 0.001));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NET.A.2019.Borovsky.02/NET.A.2019.Borovsky.02/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("NET.A.2019.Borovsky.02")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NET.A.2019.Borovsky.02")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("a5866559-4512-48ed-b07a-2f97fa3f510b")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
