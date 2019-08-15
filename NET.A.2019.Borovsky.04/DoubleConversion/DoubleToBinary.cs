88d6b520ccd6148e559dfc0e6fd2468ca73960da
Vlad Borovsky <xvorob@gmail.com>
2019-08-15 20:26:55 +0300

    Completed tasks

---
+++ NET.A.2019.Borovsky.04/DoubleConversion/DoubleToBinary.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace DoubleConversion
+{
+    public class DoubleToBinary
+    {
+        /// <summary>
+        /// Method converts number wtih floating point into binary form with double precision.
+        /// </summary>
+        /// <param name="inputDouble"> Double number to parse </param>
+        /// <returns> String with binary form of input number </returns>
+        public static string Convert(double inputDouble)
+        {
+            string signString = "", exponentString = "", mantissaString = "";
+            int exponentInt, count = 0;
+            bool firstZero = true;
+
+            if (inputDouble < 0)
+            {
+                signString += "1";
+                inputDouble = -inputDouble;
+            }
+            else
+            {
+                signString += "0";
+            }
+
+            exponentInt = (int)inputDouble;
+            inputDouble = inputDouble - exponentInt;
+            
+            while (exponentInt / 2 > 0)
+            {
+                if (!firstZero)
+                {
+                    if (exponentInt % 2 == 0)
+                    {
+                        mantissaString += "0";
+                    }
+                    else
+                    {
+                        mantissaString += "1";
+                    }
+                }
+                else
+                {
+                    if (exponentInt % 2 != 0)
+                    {
+                        mantissaString += "1";
+                        firstZero = false;
+                    }
+                }
+                exponentInt /= 2;
+                count++;
+            }
+
+            char[] temp = mantissaString.ToCharArray();
+            Array.Reverse(temp);
+            mantissaString = new string(temp);
+
+            while (inputDouble != 0)
+            {
+                inputDouble *= 2;
+                if (inputDouble >= 1)
+                {
+                    mantissaString += "1";
+                    --inputDouble;
+                }
+                else
+                {
+                    mantissaString += "0";
+                }
+            }
+
+            exponentInt = count + 1023;
+
+            firstZero = true;
+
+            while (exponentInt / 2 > 0)
+            {
+                if (!firstZero)
+                {
+                    if (exponentInt % 2 == 0)
+                    {
+                        exponentString += "0";
+                    }
+                    else
+                    {
+                        exponentString += "1";
+                    }
+                }
+                else
+                {
+                    if (exponentInt % 2 != 0)
+                    {
+                        firstZero = false;
+                        exponentString += "1";
+                    }
+                }
+                exponentInt /= 2;
+            }
+            exponentString += "1";
+
+            temp = exponentString.ToCharArray();
+            Array.Reverse(temp);
+            exponentString = new string(temp);
+
+            for (int i = exponentString.Length; i < 11; i++)
+            {
+                exponentString += "0";
+            }
+            for (int i = mantissaString.Length; i < 52; i++)
+            {
+                mantissaString += "0";
+            }
+
+            return signString + exponentString + mantissaString;
+        }
+    }
+}
---
+++ NET.A.2019.Borovsky.04/GCD/GCD_Calculations.cs
@@ -0,0 +1,179 @@
+﻿using System.Diagnostics;
+
+namespace GCD
+{
+    public class GCD_Calculations
+    {
+        /// <summary>
+        /// Library contains 4 methods to calculate GCD. Two of them provide
+        /// ability to calculate time needed for calculations, returning it in ticks.
+        /// 
+        /// Euclidean method calcuates GCD by division.
+        /// </summary>
+        /// <param name="ticks"> Ticks needed to calculate GCD </param>
+        /// <param name="inputNumbers"> Input numbers </param>
+        /// <returns> GCD (int32 format) and ticks </returns>
+        
+        public static int Euclidean(out long ticks, params int[] inputNumbers)
+        {
+            var watch = Stopwatch.StartNew();
+            int gcd = Set(true, inputNumbers);
+            watch.Stop();
+            ticks = watch.ElapsedTicks;
+            return gcd;
+        }
+
+        public static int Euclid(params int[] numbers)
+        {
+            return Set(true, numbers); 
+        }
+
+        /// <summary>
+        /// "Binary" method calculates GCD with the help of binary operations.
+        /// </summary>
+
+        public static int Binary(out long ticks, params int[] numbers)
+        {
+            var watch = Stopwatch.StartNew();
+            int gcd = Set(false, numbers);
+            watch.Stop();
+            ticks = watch.ElapsedTicks;
+            return gcd;
+        }
+
+        public static int Binary(params int[] numbers)
+        {
+            return Set(false, numbers);
+        }
+
+        /// <summary>
+        /// Setting array to appropriate - sorting and then calling selected by user calculation
+        /// </summary>
+        /// <param name="isEuclid"> Selector of the calculation method </param>
+        /// <param name="numbers"> Input numbers </param>
+        /// <returns> GCD of array </returns>
+        static int Set(bool isEuclid, params int[] numbers)
+        {
+            int[] opArray = new int[numbers.Length];
+            numbers.CopyTo(opArray, 0);
+
+            foreach (var num in opArray)
+            {
+                if (num == 0)
+                {
+                    return 0;
+                }
+            }
+
+            for (int i = 0; i < opArray.Length; i++)
+            {
+                if (opArray[i] < 0)
+                {
+                    opArray[i] = -opArray[i];
+                }
+            }
+
+            int temp1;
+            for (int i = 0; i < opArray.Length - 1; i++)
+            {
+                for (int k = i + 1; k < opArray.Length; k++)
+                {
+                    if (opArray[i] > opArray[k])
+                    {
+                        temp1 = opArray[i];
+                        opArray[i] = opArray[k];
+                        opArray[k] = temp1;
+                    }
+                }
+            }
+
+            int endOfArray = opArray.Length - 1;
+
+            for (int i = 0; i + 1 < opArray.Length; i++)
+            {
+                for (int k = 0; k + 1 < opArray.Length; k++)
+                {
+                    if (opArray[k + 1] != 0)
+                    {
+                        if (isEuclid)
+                        {
+                            opArray[k] = EuclidCalculation(opArray[k], opArray[k + 1]);
+                        }
+                        else
+                        {
+                            opArray[k] = BinaryCalculation(opArray[k], opArray[k + 1]);
+                        }
+                    }
+                }
+                opArray[endOfArray--] = 0;
+            }
+
+            return opArray[0];
+        }
+
+        /// <summary>
+        /// Euclidean calculation by division
+        /// </summary>
+        /// <param name="a"> First int </param>
+        /// <param name="b"> Second int </param>
+        /// <returns> GCD(a,b) </returns>
+        static int EuclidCalculation(int a, int b)
+        {
+            while (true)
+            {
+                if (a == 0 || b == 0)
+                {
+                    return 0;
+                }
+                else if (a % b == 0)
+                {
+                    return b;
+                }
+                else
+                {
+                    int temp = a;
+                    a = b;
+                    b = temp % b;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Binary calculation
+        /// </summary>
+        /// <param name="a"> First int </param>
+        /// <param name="b"> Second int </param>
+        /// <returns> GCD(a,b) </returns>
+        static int BinaryCalculation(int a, int b)
+        {
+            if (a == 0 || b == 0)
+            {
+                return 0;
+            }
+            else if (a == b)
+            {
+                return a;
+            }
+            if (a % 2 == 0)
+            {
+                if (b % 2 != 0)
+                {
+                    return BinaryCalculation(a >> 1, b);
+                }
+                else
+                {
+                    return BinaryCalculation(a >> 1, b >> 1) << 1;
+                }
+            }
+            if (b % 2 == 0)
+            {
+                return BinaryCalculation(a, b >> 1);
+            }
+            if (a > b)
+            {
+                return BinaryCalculation((a - b) >> 1, b);
+            }
+            return BinaryCalculation((b - a) >> 1, a);
+        }
+    }
+}
