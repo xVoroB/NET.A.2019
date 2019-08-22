e8b2666271cfdcddae60cbaea4cbce1ac18b4925
Vlad Borovsky <xvorob@gmail.com>
2019-08-22 15:45:09 +0300

    All steps except in-memory

--- NET.A.2019.Borovsky.ConsoleProject/project/ConsoleMiniProject/Library.cs
+++ NET.A.2019.Borovsky.ConsoleProject/project/ConsoleMiniProject/Library.cs
@@ -8,110 +8,140 @@ namespace ConsoleMiniProject
 {
     public class Library
     {
-            public string firstName { get; set; }
-            public string lastName { get; set; }
-            public string birthDate { get; set; }
-            public int number { get; set; }
-            static int counter;
-            public Library()
-            {
-                counter++;
-            }
+        public string firstName { get; private set; }
+        public string lastName { get; private set; }
+        public string birthDate { get; private set; }
+        public int number { get; set; }
+        static int counter;
+        public Library()
+        {
+            counter++;
+        }
 
-            public void Create()
-            {
-                Console.WriteLine("Enter first name");
-                string input = Console.ReadLine();
-                //check
-                SetNames(input, 1);
-                Console.WriteLine("Enter last name");
-                input = Console.ReadLine();
-                //check
-                SetNames(input, 2);
-                Console.WriteLine("Enter date of birth");
-                input = Console.ReadLine();
-                //check
-                SetNames(input, 3);
-            }
+        public Library(int index, string firstName, string lastName, string birthDate)
+        {
+            counter++;
+            number = index;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birthDate = birthDate;
+        }
 
-            public void SetNames(string input, int whichInput)
-            {
-                if (whichInput == 1)
-                {
-                    firstName = input;
-                }
-                else if (whichInput == 2)
-                {
-                    lastName = input;
-                }
-                else if (whichInput == 3)
-                {
-                    birthDate = input;
-                    number = counter;
-                    Console.WriteLine("Record #{0} is created", number);
-                }
-            }
+        public void ZeroCount()
+        {
+            counter = 0;
+        }
 
-            public string GetInfo()
+        public void Import(byte ind)
+        {
+            if(ind == 1)
             {
-                return ("#" + number + ", " + firstName + ", " + lastName);
-            }
-            public int Finder(string toFind, string setter)
-            {
-                setter = setter.Trim('\"', '\'');
-                if (toFind.Equals("firstname"))
-                {
-                    if (firstName == setter)
-                    {
-                        return number;
-                    }
-                }
-                else if (toFind.Equals("lastname"))
-                {
-                    if (lastName == setter)
-                    {
-                        return number;
-                    }
-                }
-                return 0;
-            }
 
-            public void Edit()
+            }
+            else if (ind == 2)
             {
-                GetInfo();
 
-                Console.WriteLine("Enter firstname");
-                string input = Console.ReadLine();
-                //check
+            }
+            else if (ind == 3)
+            {
+
+            }
+        }
+
+        public void Create()
+        {
+            Console.WriteLine("Enter first name");
+            string input = Console.ReadLine();
+            //check
+            SetNames(input, 1);
+            Console.WriteLine("Enter last name");
+            input = Console.ReadLine();
+            //check
+            SetNames(input, 2);
+            Console.WriteLine("Enter date of birth");
+            input = Console.ReadLine();
+            //check
+            SetNames(input, 3);
+        }
+
+        public void SetNames(string input, int whichInput)
+        {
+            if (whichInput == 1)
+            {
                 firstName = input;
-                Console.WriteLine("Enter lastname");
-                input = Console.ReadLine();
-                //check
-                lastName = input;
-                Console.WriteLine("Enter date of birth");
-                input = Console.ReadLine();
-                //check
-                birthDate = input;
-
-                Console.WriteLine("Record #{0} is edited", number);
             }
-
-            public string ExtendedList(string finder)
+            else if (whichInput == 2)
             {
-                if (finder.Equals("firstname"))
-                {
-                    return firstName;
-                }
-                else if (finder.Equals("id"))
-                {
-                    return ("#" + number.ToString());
-                }
-                else if (finder.Equals("lastname"))
-                {
-                    return lastName;
-                }
-                return "";
+                lastName = input;
             }
-    }
+            else if (whichInput == 3)
+            {
+                birthDate = input;
+                number = counter;
+                Console.WriteLine("Record #{0} is created", number);
+            }
+        }
+
+        public string GetInfo()
+        {
+            return ("#" + number + ", " + firstName + ", " + lastName);
+        }
+        public int Finder(string toFind, string setter)
+        {
+            setter = setter.Trim('\"', '\'');
+            if (toFind.Equals("firstname"))
+            {
+                if (firstName == setter)
+                {
+                    return number;
+                }
+            }
+            else if (toFind.Equals("lastname"))
+            {
+                if (lastName == setter)
+                {
+                    return number;
+                }
+            }
+            return 0;
+        }
+
+        public void Edit()
+        {
+            GetInfo();
+
+            Console.WriteLine("Enter firstname");
+            string input = Console.ReadLine();
+            //check
+            firstName = input;
+            Console.WriteLine("Enter lastname");
+            input = Console.ReadLine();
+            //check
+            lastName = input;
+            Console.WriteLine("Enter date of birth");
+            input = Console.ReadLine();
+            //check
+            birthDate = input;
+
+            Console.WriteLine("Record #{0} is edited", number);
+        }
+
+        public string ExtendedList(string finder)
+        {
+            if (finder.Equals("firstname"))
+            {
+                return firstName;
+            }
+            else if (finder.Equals("id"))
+            {
+                return ("#" + number.ToString());
+            }
+            else if (finder.Equals("lastname"))
+            {
+                return lastName;
+            }
+            return "";
+        }
+    }   
 
 }
--- NET.A.2019.Borovsky.ConsoleProject/project/ConsoleMiniProject/Program.cs
+++ NET.A.2019.Borovsky.ConsoleProject/project/ConsoleMiniProject/Program.cs
@@ -17,8 +17,18 @@ namespace ConsoleMiniProject
             var users = new List<Library>();
             int i = 0;
             
+
             while (true)
             {
+                var fstream = new FileStream(@"D:\buffer.txt", FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
+                var sw = new StreamWriter(fstream, Encoding.UTF8);
+                foreach (var item in users)
+                {
+                    sw.WriteLine(item.GetInfo());
+                }
+                sw.Flush();
+                sw.Dispose();
+
                 input = Console.ReadLine();
                 string[] command = input.ToLower().Split(' ');
                 switch (command[0])
@@ -124,19 +134,21 @@ namespace ConsoleMiniProject
                                 settings.NewLineOnAttributes = true;
                                 settings.ConformanceLevel = ConformanceLevel.Auto;
 
-                                using (var xmlWriter = XmlWriter.Create("users.xml", settings))
+                                using (var xmlWriter = XmlWriter.Create("D:\\consoleprojectexport\\users.xml", settings))
                                 {
+                                    xmlWriter.WriteStartElement("Users");
                                     foreach (var item in users)
                                     {
+                                        string idString = item.number.ToString();
                                         xmlWriter.WriteStartElement("User");
-                                        xmlWriter.WriteElementString("Id", item.number.ToString());
+                                        xmlWriter.WriteAttributeString("id", idString);
                                         xmlWriter.WriteElementString("Firstname", item.firstName);
                                         xmlWriter.WriteElementString("Lastname", item.lastName);
                                         string aaa = item.birthDate.ToString();
                                         xmlWriter.WriteElementString("Birth", aaa);
                                         xmlWriter.WriteEndElement();
                                     }
-
+                                    xmlWriter.WriteEndElement();
                                 }
                                 Console.WriteLine("Export .xml file finished");
                             }
@@ -154,12 +166,111 @@ namespace ConsoleMiniProject
                                 users[l].number--;
                             }
                         }
+                        i--;
                         break;
                     case "purge":
+                        fstream = new FileStream(@"D:\beforePurge.txt", FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
+                        sw = new StreamWriter(fstream, Encoding.UTF8);
+                        foreach (var item in users)
+                        {
+                            sw.WriteLine(item.GetInfo());
+                        }
+                        sw.Flush();
+                        sw.Dispose();
+
                         int b = users.Count - 1;
                         while (b >= 0)
                         {
                             users.RemoveAt(b--);
+                        }
+
+                        Console.WriteLine("All data is erased");
+                        break;
+                    case "import":
+                        if (command.Length > 1)
+                        {
+                            command[1].Trim();
+                            if (command[1].Equals("csv"))
+                            {
+                                byte index = 1;
+                                bool firstEntrance = true;
+                                if (command[2].Length > 4)
+                                {
+                                    var file = command[2].Split('.');
+                                    if (file[1].Equals("csv"))
+                                    {
+                                        using (var reader = new StreamReader("D:\\consoleprojectimport\\" + file[0] + ".csv"))
+                                        {
+                                            if (users.Count == 0)
+                                            {
+                                                users.Add(new Library());
+                                                users[0].ZeroCount();
+                                            }
+                                            else
+                                            {
+                                                users[0].ZeroCount();
+                                            }
+
+                                            users = new List<Library>();
+                                            while (!reader.EndOfStream)
+                                            {
+                                                if (!firstEntrance)
+                                                {
+                                                    var csvInput = reader.ReadLine().Split(';');
+                                                    users.Add(new Library(index, csvInput[0], csvInput[1], csvInput[2]));
+                                                    index++;
+                                                }
+                                                else
+                                                {
+                                                    var csvInput = reader.ReadLine().Split(';');
+                                                    firstEntrance = false;
+                                                }
+                                            }
+                                        }
+                                    }   
+                                
+                                }
+                                Console.WriteLine("csv import complete");
+                            }
+                            else if(command[1].Equals("xml"))
+                            {
+                                users = new List<Library> ();
+                                int ind = 0;
+                                string fName = "", lName = "", bD = "";
+                                var name = command[2].Split('.');
+                                var xDoc = new XmlDocument();
+                                xDoc.Load("D:\\consoleprojectimport\\" + name[0] + ".xml");
+                                var xRoot = xDoc.DocumentElement;
+                                foreach (XmlNode xNode in xRoot)
+                                {
+
+                                    if (xNode.Attributes.Count > 0)
+                                    {
+                                        var attr = xNode.Attributes.GetNamedItem("id");
+                                        if (attr != null)
+                                        {
+                                            ind = int.Parse(attr.Value);
+                                        }
+                                        foreach (XmlNode childNode in xNode.ChildNodes)
+                                        {
+                                            if (childNode.Name.ToLower().Equals("firstname"))
+                                            {
+                                                fName = childNode.InnerText;
+                                            }
+                                            if (childNode.Name.ToLower().Equals("lastname"))
+                                            {
+                                                lName = childNode.InnerText;
+                                            }
+                                            if (childNode.Name.ToLower().Equals("birth"))
+                                            {
+                                                bD = childNode.InnerText;
+                                            }
+                                        }
+                                        users.Add(new Library(ind, fName, lName, bD));
+                                    }
+                                }
+                                Console.WriteLine("xml import complete");
+                            }
                         }
                         break;
                     default:
